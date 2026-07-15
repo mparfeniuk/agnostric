c20f843f3f4ae4499efaf340832485b58e75f254
Max Parfeniuk <mparfeniuk@gmail.com>
2026-07-15 13:06:00 +0200

    Added favicon

--- src/components/Sidebar/ProfileButton.tsx
+++ src/components/Sidebar/ProfileButton.tsx
@@ -1,6 +1,5 @@
 import { usePrimaryPage } from '@/PageManager'
 import { useNostr } from '@/providers/NostrProvider'
-import { UserIcon } from '@phosphor-icons/react'
 import SidebarItem from './SidebarItem'
 
 export default function ProfileButton({ collapse }: { collapse: boolean }) {
